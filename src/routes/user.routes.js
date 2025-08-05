3d967f8143ba7fd70372428d7bb27254d34fed2c
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-08-05 23:25:41 +0600

    Add subscription model and refresh token route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser , loginUser , logoutUser  } from "../controllers/user.controllers.js";
+import { registerUser , loginUser , logoutUser, refreshAccessToken  } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middlewares.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const router = Router();
@@ -15,6 +15,7 @@ router.route('/login').post(loginUser);
 
 // seccure route
 router.route('/logout').post(verifyJWT, logoutUser);
+router.route('/refresh-token').post(refreshAccessToken)
 
 
 export default router;
