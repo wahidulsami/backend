63042df106c7ab0bb533c8fbb1e846645691faaf
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-08-07 22:51:19 +0600

    Refactor user routes to include getUserChannelProfile and getWatchhistory, and update mongoose usage in watch history retrieval

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,6 +4,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { uploadCloudinary } from "../utils/cloudnary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const genrateAccessAndRefreshToken = async (userId) => {
   try {
@@ -316,7 +317,7 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
 
 // todo end
 
-const getCurrentChannel = asyncHandler(async (req, res) => {
+const getUserChannelProfile = asyncHandler(async (req, res) => {
   const {username} = req.params;
   if (!username) {
     throw new ApiError(400, "username is required");
@@ -383,10 +384,51 @@ const getWatchhistory = asyncHandler(async (req, res) => {
   const user = await  User.aggregate([
     {
       $match:{
-        _id: req.user?._id
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },{
+              $addFields:{
+                owner:{
+                  $first: "$owner"
+                }
+              }
+          }
+        ]
       }
     }
   ])
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+    200,
+    user[0].watchHistory,
+    "Watch history fetched successfully"));
 })
 
 export {
@@ -399,5 +441,6 @@ export {
   updateAccoutDetails,
   updateUserAvatar,
   updateUserCoverImage,
-  getCurrentChannel
+  getUserChannelProfile,
+  getWatchhistory
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,24 +1,43 @@
 import { Router } from "express";
-import { registerUser , loginUser , logoutUser, refreshAccessToken
-   , changeCurrentPassword , getCurrentUser  } from "../controllers/user.controllers.js";
-import {upload} from "../middlewares/multer.middlewares.js"
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccoutDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchhistory,
+} from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const router = Router();
 
-router.route('/register').post(
+router.route("/register").post(
   upload.fields([
-    { name: 'avatar', maxCount: 1 },
-    { name: 'coverImage', maxCount: 1 }
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
   ]),
   registerUser
 );
-router.route('/login').post(loginUser);
+router.route("/login").post(loginUser);
 
 // seccure route
-router.route('/logout').post(verifyJWT, logoutUser);
-router.route('/refresh-token').post(refreshAccessToken)
-router.route('/change-password').post(verifyJWT, changeCurrentPassword)
-router.route("/current-user").get(verifyJWT, getCurrentUser)
-
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account-details").patch(verifyJWT, updateAccoutDetails);
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/update-cover")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchhistory);
 
 export default router;
